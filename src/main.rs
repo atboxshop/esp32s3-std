823d0f683b7608baec47f077338f68ccd7cd5926
atboxshop <atboxshop@gmail.com>
2023-03-06 02:07:55 -0500

    456

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
-//use esp_idf_hal::adc::config::Config;
-//use esp_idf_hal::adc::*;
+use esp_idf_hal::adc::config::Config;
+use esp_idf_hal::adc::*;
 use esp_idf_hal::peripherals::Peripherals;
 
 fn main()
 {
     let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio9).unwrap();
-
-
+    //let mut led = PinDriver::output(peripherals.pins.gpio12).unwrap();
+    let mut adc = AdcDriver::new(peripherals.adc2, &Config::new().calibration(true)).unwrap();
+    let mut adc_pin: esp_idf_hal::adc::AdcChannelDriver<'_, Gpio13, Atten11dB<_>> =
+        AdcChannelDriver::new(peripherals.pins.gpio13).unwrap();
     loop {
-        led.set_high().unwrap();
-        // we are sleeping here to make sure the watchdog isn't triggered
-        FreeRtos::delay_ms(100);
-        led.set_low().unwrap();
-        FreeRtos::delay_ms(100);
-        println!("Hello World!");
+        //led.set_high().unwrap();
+        //we are sleeping here to make sure the watchdog isn't triggered
+        FreeRtos::delay_ms(50);
+        let value1 = adc.read(&mut adc_pin).unwrap();
+        let value2:f32 = value1.into();
+        println!("ADC value: {}", value2/1000.00);
+        //FreeRtos::delay_ms(500);
+        //led.set_low().unwrap();
+        //FreeRtos::delay_ms(100);
+        //println!("ADC value: {}", adc.read(&mut adc_pin).unwrap());
+        //FreeRtos::delay_ms(500);
+        //println!("Hello World!");
    }
 }
